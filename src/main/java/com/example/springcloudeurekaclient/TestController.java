dd3cdc33e1e5b8398529c6df3ede28d2b85dd21a
nicexy <nicepro_yx@163.com>
2019-11-05 10:23:21 +0800

    删除多余包

--- src/main/java/com/example/springcloudeurekaclient/TestController.java
+++ src/main/java/com/example/springcloudeurekaclient/TestController.java
@@ -8,8 +8,6 @@ import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
 @RestController("/")
 public class TestController {
     private Logger log = LoggerFactory.getLogger(this.getClass());
